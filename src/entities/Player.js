681b9a69d7c69d32750171b465016b02984501bf
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2022-09-13 15:25:11 +0200

    first commit

---
+++ src/entities/Player.js
@@ -0,0 +1,65 @@
+import Phaser from "phaser";
+import initAnimations from "./playerAnims";
+
+class Player extends Phaser.Physics.Arcade.Sprite{
+    constructor(scene,x,y){
+        super(scene,x,y,"player");
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+    
+
+        this.init();
+        this.initEvents();
+    }
+
+    init(){
+
+    this.gravity = 500;
+    this.playerSpeed = 200;
+    this.jumpCount = 0;
+    this.consecutiveJumps = 1;
+    this.cursors = this.scene.input.keyboard.createCursorKeys();
+
+    this.body.setGravityY(this.gravity);
+    this.setCollideWorldBounds(true);
+
+    initAnimations(this.scene.anims);
+    }
+
+    initEvents(){
+        this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update,this)
+    }
+
+    update(){
+    const {left, right, space, up} = this.cursors;
+    const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space);
+    const isSpaceJustUp = Phaser.Input.Keyboard.JustDown(up);
+    const onFloor = this.body.onFloor();
+
+    if(left.isDown){
+      this.setVelocityX(-this.playerSpeed);
+      this.setFlipX(true);
+    }else if(right.isDown){
+      this.setVelocityX(+this.playerSpeed);
+      this.setFlipX(false);
+    }else{
+      this.setVelocityX(0);
+    }
+
+    if((isSpaceJustDown || isSpaceJustUp) && (onFloor || this.jumpCount < this.consecutiveJumps)){
+        this.setVelocityY(-this.playerSpeed * 1.5);
+        this.jumpCount++; 
+    }
+
+    if(onFloor){
+        this.jumpCount = 0;
+    }
+
+    this.body.velocity.x !== 0?
+    this.play("run",true) : this.play("idle",true);
+    }
+
+
+}
+
+export default Player;
---
+++ src/scenes/Play.js
@@ -0,0 +1,41 @@
+import Phaser from 'phaser';
+import Player from "../entities/Player";
+
+class Play extends Phaser.Scene {
+
+  constructor() {
+    super('PlayScene');
+  }
+
+  create() {
+    const map = this.createMap();
+    const layers = this.createLayers(map);
+    const player = this.createPlayer();
+    
+    this.physics.add.collider(player,layers.platformsColliders);
+  }
+
+  createMap(){
+    const map = this.make.tilemap({key: 'map'});
+    map.addTilesetImage('main_lev_build_1', 'tiles-1');
+    return map;
+  }
+
+  createLayers(map){
+    const tileset = map.getTileset('main_lev_build_1');
+    const platformsColliders = map.createDynamicLayer('platforms_colliders', tileset);
+    const enviroment = map.createStaticLayer('environment', tileset);
+    const platforms = map.createDynamicLayer('platforms', tileset);
+    
+
+    platformsColliders.setCollisionByProperty({collides: true});
+
+    return {enviroment, platforms, platformsColliders};
+  }
+
+  createPlayer(){
+    return new Player(this, 100, 250);
+  }
+}
+
+export default Play;
