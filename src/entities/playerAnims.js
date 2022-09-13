01e09ecfe9a5587835800a0738955ef0b360cba2
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2022-09-13 15:36:07 +0200

    jump

--- src/entities/playerAnims.js
+++ src/entities/playerAnims.js
@@ -3,6 +3,14 @@
 export default (anims) => {
 
     anims.create({
+        key:"jump",
+        frames: anims.generateFrameNumbers("player",{start:17,end:23}),
+        frameRate:2,
+        repeat: 1
+    })
+
+
+    anims.create({
         key:"idle",
         frames: anims.generateFrameNumbers("player",{start:0,end:8}),
         frameRate:9,
