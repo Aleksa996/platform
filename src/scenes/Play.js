48c7374b42b283a8edc7f55a90be5af69aec81b7
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2022-09-16 14:30:16 +0200

    player zones

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,15 +3,24 @@ import Player from "../entities/Player";
 
 class Play extends Phaser.Scene {
 
-  constructor() {
+  constructor(config) {
     super('PlayScene');
+    this.config = config;
   }
 
   create() {
     const map = this.createMap();
     const layers = this.createLayers(map);
-    const player = this.createPlayer();
-    player.addCollider(layers.platformsColliders);
+    const playerZones = this.getPlayerZones(layers.playerZones);
+    const player = this.createPlayer(playerZones);
+    
+    this.createPlayerColliders(player,{
+      colliders:{
+      platformsColliders:layers.platformsColliders
+    }});
+
+    this.createEndOfLevel(playerZones.end);
+    this.setupFollowupCameraOn(player);
   }
 
   createMap(){
@@ -25,15 +34,41 @@ class Play extends Phaser.Scene {
     const platformsColliders = map.createDynamicLayer('platforms_colliders', tileset);
     const enviroment = map.createStaticLayer('environment', tileset);
     const platforms = map.createDynamicLayer('platforms', tileset);
+    const playerZones = map.getObjectLayer("player_zones");
     
 
     platformsColliders.setCollisionByProperty({collides: true});
 
-    return {enviroment, platforms, platformsColliders};
+    return {enviroment, platforms, platformsColliders,playerZones};
   }
 
-  createPlayer(){
-    return new Player(this, 100, 250);
+  createPlayer({start}){
+    return new Player(this, start.x, start.y);
+  }
+
+  createPlayerColliders(player,{colliders}){
+    player.addCollider(colliders.platformsColliders);
+  }
+
+  setupFollowupCameraOn(player){
+    const {height,width,mapOffset,zoomFactor} = this.config;
+
+    this.physics.world.setBounds(0,0,width + mapOffset,height + 200);
+
+    this.cameras.main.setBounds(0,0,width + mapOffset,height).setZoom(zoomFactor);
+      this.cameras.main.startFollow(player);
+  }
+
+  getPlayerZones(playerZone){
+    const playerZones = playerZone.objects;
+    return{
+      start: playerZones[0],
+      end: playerZones[1]
+    }
+  }
+
+  createEndOfLevel(end){
+    this.physics.add.sprite(end.x,end.y,"end").setSize(5, 200)
   }
 
   
